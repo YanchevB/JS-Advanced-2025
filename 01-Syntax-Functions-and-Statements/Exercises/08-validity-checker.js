a6cb2556b856f1ee94a6dc7d12d130aac85a5106
Boyan Yanchev <byanchev6@gmail.com>
2025-09-16 13:58:12 +0300

    Solve exercise 8

---
+++ 01-Syntax-Functions-and-Statements/Exercises/08-validity-checker.js
@@ -0,0 +1,25 @@
+function validityChecker(x1, y1, x2, y2) {
+  let d1 = Math.sqrt(x1 ** 2 + y1 ** 2);
+  let d2 = Math.sqrt(x2 ** 2 + y2 ** 2);
+  let d3 = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+
+  if (Number.isInteger(d1)) {
+    console.log(`{${x1}, ${y1}} to {0, 0} is valid`);
+  } else {
+    console.log(`{${x1}, ${y1}} to {0, 0} is invalid`)
+  }
+
+  if (Number.isInteger(d2)) {
+    console.log(`{${x2}, ${y2}} to {0, 0} is valid`);
+  } else {
+    console.log(`{${x2}, ${y2}} to {0, 0} is invalid`)
+  }
+
+  if (Number.isInteger(d3)) {
+    console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is valid`);
+  } else {
+    console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is invalid`)
+  }
+}
+
+validityChecker(3, 0, 0, 4);
