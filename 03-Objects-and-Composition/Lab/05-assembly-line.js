7847da7bd40debedc4bf408a6fda94eb897f605c
Boyan Yanchev <byanchev6@gmail.com>
2025-09-21 20:24:10 +0300

    Solve lab exercise 5

---
+++ 03-Objects-and-Composition/Lab/05-assembly-line.js
@@ -0,0 +1,71 @@
+function createAssemblyLine() {
+  const library = {
+    hasClima(object) {
+      object.temp = 21;
+      object.tempSettings = 21;
+      object.adjustTemp = () => {
+        if (object.temp < object.tempSettings) {
+          object.temp += 1;
+        } else if (object.temp > object.tempSettings) {
+          object.temp -= 1;
+        }
+      }
+      return object;
+    },
+
+    hasAudio(object) {
+      object.currentTrack = {
+        name: null,
+        artist: null
+      }
+      object.nowPlaying = () => {
+        if (object.currentTrack) {
+          console.log(`Now playing '${object.currentTrack.name}' by ${object.currentTrack.artist}`);
+        }
+      }
+      return object;
+    },
+
+    hasParktronic(object) {
+      object.checkDistance = (distance) => {
+        if (distance < 0.1) {
+          console.log("Beep! Beep! Beep!");
+        } else if (distance < 0.25) {
+          console.log("Beep! Beep!");
+        } else if (distance < 0.5) {
+          console.log("Beep!");
+        } else {
+          console.log('');
+        }
+      }
+      return object;
+    }
+  }
+
+  return library;
+}
+
+const assemblyLine = createAssemblyLine();
+const myCar = {
+  make: 'Toyota',
+  model: 'Avensis'
+};
+
+assemblyLine.hasClima(myCar);
+console.log(myCar.temp);
+myCar.tempSettings = 18;
+myCar.adjustTemp();
+console.log(myCar.temp);
+
+assemblyLine.hasAudio(myCar);
+myCar.currentTrack = {
+  name: 'Never Gonna Give You Up',
+  artist: 'Rick Astley'
+};
+myCar.nowPlaying();
+
+assemblyLine.hasParktronic(myCar);
+myCar.checkDistance(0.4);
+myCar.checkDistance(0.2);
+
+console.log(myCar);
