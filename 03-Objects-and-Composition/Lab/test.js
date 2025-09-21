d69dcccf2ac53be023f0bf4019143f52ede3ef12
Boyan Yanchev <byanchev6@gmail.com>
2025-09-21 21:39:06 +0300

    Create a file to test JS function from lab exercise 6

---
+++ 03-Objects-and-Composition/Lab/test.js
@@ -0,0 +1,43 @@
+function fromJSONToHTMLTable(input) {
+  const data = JSON.parse(input);
+  
+  if (data.length === 0) return console.log('<table></table>');
+
+  const headers = getHeaders(data);
+  const rows = getDataRows(data);
+  const table = `<table>\n${headers}\n${rows}</table>`;
+  console.log(table);
+
+  function escapeHTML(text) {
+    return String(text)
+      .replace(/&/g, '&amp;')
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;')
+      .replace(/"/g, '&quot;')
+      .replace(/'/g, '&apos;')
+  }
+  
+  function getHeaders(data) {
+    const keys = Object.keys(data[0])
+    let row = ' <tr>';
+    for (let key of keys) {
+      row += `<th>${escapeHTML(key)}</th>`
+    }
+    row += '</tr>';
+    return row;
+  }
+
+  function getDataRows(data) {
+    let rows = '';
+    for (let obj of data) {
+      rows += ' <tr>'
+      for (let value of Object.values(obj)) {
+        rows += `<td>${escapeHTML(value)}</td>`;
+      }
+      rows += '</tr>\n'
+    }
+    return rows;
+  }
+}
+
+fromJSONToHTMLTable(['[{"Name":"Stamat","Price":5.5},{"Name":"Rumen","Price":6}]']);
