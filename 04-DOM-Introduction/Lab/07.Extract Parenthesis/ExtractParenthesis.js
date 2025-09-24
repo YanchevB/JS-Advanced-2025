02dcb66eecad85e010e40d832a32a1509869491c
Boyan Yanchev <byanchev6@gmail.com>
2025-09-24 14:38:08 +0300

    Solve lab exercise 7

--- 04-DOM-Introduction/Lab/07.Extract Parenthesis/ExtractParenthesis.js
+++ 04-DOM-Introduction/Lab/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,15 @@
 function extract(content) {
+  let paragraph = document.getElementById(content).textContent;
+  let pattern = /\(([^)]+)\)/g;
+  let result = [];
 
+  let match = pattern.exec(paragraph);
+  while (match) {
+    console.log(match);
+    result.push(match[1]);
+    match = pattern.exec(paragraph);
+  }
+
+  result = result.join('; ');
+  return result;
 }
