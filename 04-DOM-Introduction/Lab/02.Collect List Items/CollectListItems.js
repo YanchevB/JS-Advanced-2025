520ccaa144236876651316d8f04fab9158e25145
Boyan Yanchev <byanchev6@gmail.com>
2025-09-24 13:16:29 +0300

    Solve lab exercise 2

--- 04-DOM-Introduction/Lab/02.Collect List Items/CollectListItems.js
+++ 04-DOM-Introduction/Lab/02.Collect List Items/CollectListItems.js
@@ -1,3 +1,6 @@
 function extractText() {
-    // TODO
+    const main = Array.from(document.getElementsByTagName('li'));
+    const text = main.map(el => el.textContent).join('\n');
+    const textArea = document.getElementById('result');
+    textArea.value = text;
 }
