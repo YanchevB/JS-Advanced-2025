7741d25213416f4c16df79adf380025a94847920
Boyan Yanchev <byanchev6@gmail.com>
2025-09-26 19:02:24 +0300

    Solve exercise 2

--- 04-DOM-Introduction/Exercises/02. Pascal or Camel Case/solution.js
+++ 04-DOM-Introduction/Exercises/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,45 @@
 function solve() {
-  //TODO...
+  const textRef = document.getElementById('text');
+  const namingConventionRef = document.getElementById('naming-convention');
+  const resultRef = document.getElementById('result');
+
+  let input = textRef.value;
+  const namingConvention = namingConventionRef.value;
+
+  switch (namingConvention) {
+    case 'Pascal Case':
+      resultRef.textContent = transformToPascal(input);
+      break;
+    
+    case 'Camel Case':
+      resultRef.textContent = transformToCamel(input);
+      break;
+
+    default: resultRef.textContent = 'Error!'
+      break;
+  }
+
+  function transformToPascal(input) {
+    input = input.toLowerCase().split(' ');
+    let output = [];
+
+    for (let i = 0; i < input.length; i++) {
+      let currentWord = input[i];
+      let result = currentWord.charAt(0).toUpperCase() + currentWord.slice(1);
+      output.push(result);
+    }
+    return output.join('');
+  }
+
+  function transformToCamel(input) {
+    input = input.toLowerCase().split(' ');
+    let output = [];
+    output = [input[0]];
+    for (let i = 1; i < input.length; i++) {
+      let currentWord = input[i];
+      let result = currentWord.charAt(0).toUpperCase() + currentWord.slice(1);
+      output.push(result);
+    }
+    return output.join('')
+  }
 }
