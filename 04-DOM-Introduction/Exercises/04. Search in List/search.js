4943577403fcac439281a894d812e46fa018569c
Boyan Yanchev <byanchev6@gmail.com>
2025-09-28 19:52:20 +0300

    Solve exercise 4

--- 04-DOM-Introduction/Exercises/04. Search in List/search.js
+++ 04-DOM-Introduction/Exercises/04. Search in List/search.js
@@ -1,3 +1,21 @@
 function search() {
-   // TODO
+   const listRef = document.querySelectorAll('ul li');
+   const searchRef = document.getElementById('searchText');
+   const match = searchRef.value;
+   const resultRef = document.getElementById('result');
+
+   const listArr = Array.from(listRef);
+   let result = [];
+
+   for (let line of listArr) {
+      if (!line.textContent.includes(match)) {
+         continue;
+      }
+      result.push(line.textContent);
+      line.style.fontWeight = 'bolder';
+      line.style.textDecoration = 'underline'
+   }
+
+   resultRef.textContent = `${result.length} matches found`;
+   result = [];
 }
