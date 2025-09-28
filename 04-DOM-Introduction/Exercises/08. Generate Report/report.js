380234f6ce6e29993ff2d240fc811605bb53b532
Boyan Yanchev <byanchev6@gmail.com>
2025-09-28 22:01:41 +0300

    Start solving exercise 8

--- 04-DOM-Introduction/Exercises/08. Generate Report/report.js
+++ 04-DOM-Introduction/Exercises/08. Generate Report/report.js
@@ -1,3 +1,18 @@
 function generateReport() {
-    //TODO
+    const keys = Array.from(document.querySelectorAll('tr th'))
+        .map(key => key.textContent.toLowerCase().trim());
+
+    const entries = [];
+    const entriesArr = Array.from(document.querySelectorAll('tbody tr'))
+    
+
+    const rows = [];
+
+    function createObj(keys, entries) {
+        const obj = {};
+        if (!obj.hasOwnProperty(keys)) {
+            obj[keys] = entries
+        }
+        rows.push(obj);
+    }
 }
