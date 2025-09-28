a02ba37a49ee593667cf69855466b8ce45bdfa73
Boyan Yanchev <byanchev6@gmail.com>
2025-09-28 21:07:15 +0300

    Solve exercise 6

--- 04-DOM-Introduction/Exercises/06. Format the Text/solution.js
+++ 04-DOM-Introduction/Exercises/06. Format the Text/solution.js
@@ -1,3 +1,20 @@
 function solve() {
-  //TODO
+  const inputRef = document.getElementById('input');
+  const outputRef = document.getElementById('output');
+
+  const sentences = inputRef.value.split('.').filter(el => !!el && el !== ' ');
+
+  for (let i = 0; i < sentences.length; i += 3) {
+    let result = [];
+
+    for (let x = 0; x < 3; x++) {
+      if (!sentences[i + x]) {
+        break;
+      }
+      result.push(sentences[i + x]);
+    }
+
+    let buff = result.join('.') + '.';
+    outputRef.innerHTML += `<p>${buff.trim()}</p>`;
+  }
 }
