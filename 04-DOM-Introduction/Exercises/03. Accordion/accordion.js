c413886d9c5e6ac2aa5fff7b8f4d1561eb2c8989
Boyan Yanchev <byanchev6@gmail.com>
2025-09-28 19:11:30 +0300

    Solve exercise 3

--- 04-DOM-Introduction/Exercises/03. Accordion/accordion.js
+++ 04-DOM-Introduction/Exercises/03. Accordion/accordion.js
@@ -1,3 +1,12 @@
 function toggle() {
-    console.log('TODO:...');
+    const btnRef = document.getElementsByClassName('button');
+    const moreTextRef = document.getElementById('extra');
+    
+    if (btnRef[0].textContent === 'Less') {
+        btnRef[0].textContent = 'More';
+        moreTextRef.style.display = 'none';
+    } else {
+        moreTextRef.style.display = 'block';
+        btnRef[0].textContent = 'Less';
+    }
 }
