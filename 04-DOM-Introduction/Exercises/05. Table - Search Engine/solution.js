3b5bb65b50545104bcfdcd368577ba670c4695bf
Boyan Yanchev <byanchev6@gmail.com>
2025-09-28 20:27:30 +0300

    Solve exercise 5

--- 04-DOM-Introduction/Exercises/05. Table - Search Engine/solution.js
+++ 04-DOM-Introduction/Exercises/05. Table - Search Engine/solution.js
@@ -2,7 +2,23 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+      const tableRef = document.querySelectorAll('tbody tr');
+      const searchRef = document.getElementById('searchField');
+      const dataArr = []
+      tableRef.forEach(row => dataArr.push(row));
 
+      for (let row of dataArr) {
+         if (!searchRef.value) {
+            return;
+         }
+         row.classList.remove('select');
+         const matchedRow = row.textContent.includes(searchRef.value);
+         
+         if (matchedRow) {
+            row.classList.add('select');
+         }
+      }
+
+      searchRef.value = '';
    }
 }
