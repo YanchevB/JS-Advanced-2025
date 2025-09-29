a49e6d12d96ae7abe3ddcb5d567b39e00236e41e
Boyan Yanchev <byanchev6@gmail.com>
2025-09-29 17:13:43 +0300

    Solve lab exercise 2

--- 05-DOM-Manipulations-and-Events/Lab/02. Delete-From-Table/app.js
+++ 05-DOM-Manipulations-and-Events/Lab/02. Delete-From-Table/app.js
@@ -1,3 +1,16 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    const inputRef = document.getElementsByTagName('input');
+    const input = inputRef[0].value;
+    
+    const rows = Array.from(document.querySelector('tbody').children);
+
+    for (let row of rows) {
+        let email = row.children[1].textContent;
+        if (email === input) {
+            row.remove();
+            document.getElementById('result').textContent = 'Deleted.'
+        } else {
+            document.getElementById('result').textContent = 'Not found.'
+        }
+    }
 }
