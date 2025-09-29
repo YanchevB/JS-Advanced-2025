75112cb21f3512941902f82bcdae0795ee7be011
Boyan Yanchev <byanchev6@gmail.com>
2025-09-29 17:02:08 +0300

    Solve lab exercise 1

--- 05-DOM-Manipulations-and-Events/Lab/01. List-Of-Items/app.js
+++ 05-DOM-Manipulations-and-Events/Lab/01. List-Of-Items/app.js
@@ -1,3 +1,9 @@
 function addItem() {
-    console.log('TODO:...');
+    const inputRef = document.getElementById('newItemText');
+
+    const item = document.createElement('li');
+    item.textContent = inputRef.value;
+
+    const list = document.getElementById('items');
+    list.appendChild(item);
 }
