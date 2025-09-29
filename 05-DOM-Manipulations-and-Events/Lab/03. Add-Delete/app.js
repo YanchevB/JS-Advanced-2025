5fbfdea0d318062184d925bcd592c1eb659e81da
Boyan Yanchev <byanchev6@gmail.com>
2025-09-29 17:30:59 +0300

    Solve lab exercise 3

--- 05-DOM-Manipulations-and-Events/Lab/03. Add-Delete/app.js
+++ 05-DOM-Manipulations-and-Events/Lab/03. Add-Delete/app.js
@@ -1,3 +1,27 @@
 function addItem() {
-    //TODO...
+    const inputRef = document.getElementById('newItemText');
+
+    if (!inputRef.value) {
+        return;
+    }
+
+    const item = document.createElement('li');
+    item.textContent = inputRef.value;
+
+    const deleteBtn = document.createElement('a');
+    deleteBtn.href = '#';
+    deleteBtn.textContent = '[Delete]';
+    item.appendChild(deleteBtn);
+
+    deleteBtn.addEventListener('click', onDelete);
+
+    function onDelete(event) {
+        const row = event.target.parentElement;
+        row.remove();        
+    }
+
+    const list = document.getElementById('items');
+    list.appendChild(item);
+
+    inputRef.value = '';
 }
