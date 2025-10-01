7ccbb26ad1dc87a57b597efd4fc34e01912f74be
Boyan Yanchev <byanchev6@gmail.com>
2025-10-01 21:08:46 +0300

    Start solving lab exercise 7

---
+++ 06-Advanced-Functions/Lab/07-cars.js
@@ -0,0 +1,45 @@
+function cars(input) {
+  const objects = {};
+  const parents = {};
+
+  for (let entry of input) {
+    let [command, name, param2, param3] = entry.split(' ');
+
+    if (command === 'create') {
+      objects[name] = {};
+
+      if (param2 === 'inherit') {
+        parents[name] = param3
+      }
+
+    } else if (command === 'set') {
+      objects[name][param2] = param3;
+
+    } else if (command === 'print') {
+      const result = {};
+      
+      collectProps(name);
+
+      const output = Object.entries(result)
+        .map(([k, v]) => `${k}:${v}`)
+        .join(',');
+
+      console.log(output);
+
+      function collectProps(name) {
+        if (parents[name]) collectProps(parents[name]);
+        Object.assign(result, objects[name]);
+      }
+    }
+  }
+}
+
+
+cars([
+  'create c1',
+  'create c2 inherit c1',
+  'set c1 color red',
+  'set c2 model new',
+  'print c1',
+  'print c2'
+]);
