4cda154c866ff1ded8520022101f43528962b27a
Boyan Yanchev <byanchev6@gmail.com>
2025-10-03 22:19:44 +0300

    Solve exercise 1

--- 05-DOM-Manipulations-and-Events/Exercises/01. Sections/script.js
+++ 05-DOM-Manipulations-and-Events/Exercises/01. Sections/script.js
@@ -1,3 +1,19 @@
 function create(words) {
-   console.log('TODO:...');
+   const divRef = document.getElementById('content');
+
+   for (let i = 0; i < words.length; i++) {
+      const div = document.createElement('div');
+      const p = document.createElement('p');
+      p.textContent = words[i];
+      p.style.display = "none";
+
+      divRef.appendChild(div).appendChild(p);
+
+      div.addEventListener('click', (e) => {
+         const div = e.currentTarget;
+         const p = div.children[0];
+         let currentState = p.style.display;
+         p.style.display = currentState === "none" ? "block" : "none";
+      })
+   }
 }
