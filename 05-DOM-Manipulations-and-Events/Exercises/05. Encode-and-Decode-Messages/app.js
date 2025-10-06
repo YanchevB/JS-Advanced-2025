8db920f7565f7a01cf3a46ef78f542c45ac7355a
Boyan Yanchev <byanchev6@gmail.com>
2025-10-06 17:58:19 +0300

    Solve exercise 5

--- 05-DOM-Manipulations-and-Events/Exercises/05. Encode-and-Decode-Messages/app.js
+++ 05-DOM-Manipulations-and-Events/Exercises/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,33 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    const btns = Array.from(document.querySelectorAll('button'));
+    const encodeBtn = btns[0];
+    const decodeBtn = btns[1];
+
+    const textareasRef = Array.from(document.querySelectorAll('#main textarea'));
+    const encodeAreaValue = textareasRef[0];
+    const decodeAreaValue = textareasRef[1];
+
+    encodeBtn.addEventListener('click', encodeMessage);
+    decodeBtn.addEventListener('click', decodeMessage);
+
+    function encodeMessage() {
+        const newMessage = [];
+        for (let char of encodeAreaValue.value) {
+            const currentCharCode = char.charCodeAt(0);
+            char = String.fromCharCode(currentCharCode + 1);
+            newMessage.push(char);
+        }
+        decodeAreaValue.value = newMessage.join('');
+        encodeAreaValue.value = '';
+    }
+
+    function decodeMessage() {
+        const decodedMessage = [];
+        for (let char of decodeAreaValue.value) {
+            const currentCharCode = char.charCodeAt(0);
+            char = String.fromCharCode(currentCharCode - 1);
+            decodedMessage.push(char);
+        }
+        decodeAreaValue.value = decodedMessage.join('');
+    }
 }
