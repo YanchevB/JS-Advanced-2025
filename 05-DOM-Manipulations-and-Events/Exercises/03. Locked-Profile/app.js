37742b2816369dc281dbd8e0f650594719e9ae8a
Boyan Yanchev <byanchev6@gmail.com>
2025-10-06 14:00:13 +0300

    Solve exercise 3

--- 05-DOM-Manipulations-and-Events/Exercises/03. Locked-Profile/app.js
+++ 05-DOM-Manipulations-and-Events/Exercises/03. Locked-Profile/app.js
@@ -1,3 +1,26 @@
 function lockedProfile() {
-    console.log('TODO...')
+    const btns = document.querySelectorAll('button');
+    Array.from(btns).forEach(btn => btn.addEventListener('click', onClick));
+
+    function onClick(e) {
+        const btn = e.target;
+        const card = btn.parentElement;
+        const radioBtn = Array.from(card.querySelectorAll("input[type='radio']"))
+            .filter(x => x.checked)[0];
+        const profileState = radioBtn.value;
+        const moreInfoDiv = card.querySelector('div');
+
+        if (profileState === 'lock') {
+            return;
+        }
+
+        if (btn.textContent === 'Show more') {
+            moreInfoDiv.style.display === 'block';
+            btn.textContent = 'Hide it';
+            debugger;
+        } else {
+            moreInfoDiv.style.display === 'none';
+            btn.textContent = 'Show more';
+        }
+    }
 }
