501610d9ae1da523a3d9b7d56ef6ad7f728a10bb
Boyan Yanchev <byanchev6@gmail.com>
2025-10-06 14:24:05 +0300

    Solve exercise 4

--- 05-DOM-Manipulations-and-Events/Exercises/04. Fill-Dropdown/dropdown.js
+++ 05-DOM-Manipulations-and-Events/Exercises/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,18 @@
 function addItem() {
-    console.log('TODO:...');
+    const textInfo = document.getElementById('newItemText');
+    const valueInfo = document.getElementById("newItemValue");
+    const dropdownMenu = document.getElementById('menu');
+
+    let key = textInfo.value;
+    let value = valueInfo.value;
+
+    if (!key || !value) return;
+
+    const option = document.createElement('option');
+    option.textContent = key;
+    option.value = value;
+    dropdownMenu.appendChild(option);
+
+    textInfo.value = '';
+    valueInfo.value = '';
 }
