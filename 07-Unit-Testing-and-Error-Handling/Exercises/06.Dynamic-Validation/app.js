3e95ecbdcff24d84e040365ef0cb1f27b388d4e5
Boyan Yanchev <byanchev6@gmail.com>
2025-10-14 22:25:47 +0300

    Solve exercise 6

--- 07-Unit-Testing-and-Error-Handling/Exercises/06.Dynamic-Validation/app.js
+++ 07-Unit-Testing-and-Error-Handling/Exercises/06.Dynamic-Validation/app.js
@@ -1,3 +1,11 @@
 function validate() {
-    // TODO
+    const validationRegex = /^[a-z]+@[a-z]+\.[a-z]+$/;
+    const inputRef = document.getElementById('email');
+
+    inputRef.addEventListener('change', (e) => {
+        const input = inputRef.value;
+        const target = e.target;
+        
+        !validationRegex.test(input) ? target.classList.add('error') : target.classList.remove('error');
+    })
 }
