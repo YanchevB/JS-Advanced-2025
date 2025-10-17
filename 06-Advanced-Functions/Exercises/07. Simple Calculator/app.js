e5ef7783b6563d3cf5139dcc0f7ac0f107d31e9a
Boyan Yanchev <byanchev6@gmail.com>
2025-10-17 16:09:37 +0300

    Solve exercise 7

--- 06-Advanced-Functions/Exercises/07. Simple Calculator/app.js
+++ 06-Advanced-Functions/Exercises/07. Simple Calculator/app.js
@@ -1,7 +1,21 @@
 function calculator() {
-    // TODO:
+    let s1, s2, result;
+    return {
+        init: function(selector1, selector2, resultSelector) {
+            s1 = document.querySelector(selector1);
+            s2 = document.querySelector(selector2);
+            result = document.querySelector(resultSelector);
+        },
+
+        add: function() {
+            result.value = Number(s1.value) + Number(s2.value);
+        },
+
+        subtract: function() {
+            result.value = Number(s1.value) - Number(s2.value);
+        }
+    }
 }
 
-
-
-
+const calculate = calculator();
+calculate.init('#num1', '#num2', '#result');
