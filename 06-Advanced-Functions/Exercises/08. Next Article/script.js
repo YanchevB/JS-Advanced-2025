715384a6b5bcd622c3116d8bbb16506449a4278b
Boyan Yanchev <byanchev6@gmail.com>
2025-10-17 16:49:14 +0300

    Solve exercise 8

--- 06-Advanced-Functions/Exercises/08. Next Article/script.js
+++ 06-Advanced-Functions/Exercises/08. Next Article/script.js
@@ -1,3 +1,17 @@
 function getArticleGenerator(articles) {
-    // TODO
+    const remainingArticles = [...articles];
+
+    function generateArticle() {
+        if (remainingArticles.length === 0) {
+            return;
+        }
+
+        const divRef = document.getElementById('content');
+        const articleText = remainingArticles.shift();
+        const articleEl = document.createElement('article');
+        articleEl.textContent = articleText;
+        divRef.appendChild(articleEl);
+    }
+
+    return generateArticle;
 }
